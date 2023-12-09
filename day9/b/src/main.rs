b9000a8132740477b04eb73cdb7d018a1bdf4a68
akwasniewski <akwasniewski.pl@gmail.com>
2023-12-09 13:28:15 +0100

    day9

---
+++ day9/b/src/main.rs
@@ -0,0 +1,34 @@
+use std::io;
+fn extrapolate(values: &mut Vec<i32>){
+    let mut difs: Vec<i32> = Vec::new();
+    let mut zeros_count = 0;
+    for i in 1..values.len(){
+        difs.push(values[i]-values[i-1]);
+        if difs[i-1]==0{
+            zeros_count+=1;
+        }
+    }
+    if zeros_count==difs.len(){
+        difs.push(0);  
+    }
+    else{
+        extrapolate(&mut difs);
+        values.insert(0, values.first().unwrap()-difs.first().unwrap());
+    }
+
+}
+fn main() -> io::Result<()> {
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer)?;
+    let line_count=buffer.trim().parse().unwrap();
+    let mut sum: i32 = 0;
+    for _ in 0..line_count{
+        buffer = String::new();
+        io::stdin().read_line(&mut buffer)?;
+        let mut values: Vec<i32>=buffer.trim().split_whitespace().map(|x| x.trim().parse::<i32>().unwrap()).collect::<Vec<i32>>();
+        extrapolate(&mut values);
+        sum+=values.first().unwrap();
+    }
+    println!("{sum}");
+    Ok(())
+}
